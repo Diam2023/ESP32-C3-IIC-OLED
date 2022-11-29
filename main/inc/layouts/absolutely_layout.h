73df72db082bae101dab6d169b2dee3aea041ebe
monoliths <monoliths-uni@outlook.com>
2022-11-29 14:08:32 +0800

    rewrite absolutely_layout

--- main/inc/layouts/absolutely_layout.h
+++ main/inc/layouts/absolutely_layout.h
@@ -6,6 +6,7 @@
 #define ESP32_C3_IIC_OLED_ABSOLUTELY_LAYOUT_H
 
 #include "layout.h"
+#include <unordered_map>
 
 namespace oled
 {
@@ -13,23 +14,44 @@ namespace oled
 class AbsolutelyLayout : public Layout
 {
 private:
+    /**
+     * Use Unordered Multi Map For Save Point And Widget
+     */
+    std::unordered_multimap<Point, Widget*> m_objects;
+
 public:
+    /**
+     * Save Page For Widget And Self.
+     * @param pPage Page For Data.
+     */
+    void setPage(Page* pPage) override;
+
+    /**
+     * Flash Widgets Data For Page
+     */
+    void flash() override;
+
+    /**
+     * Only Flash Widget
+     * @param pWidget
+     */
+    void flash(const Widget* pWidget) override;
+
     /**
      * Add The Widget In Position pPoint
      * @param pWidget widget
      * @param pPoint position
      */
-    void addWidget(Widget* pWidget, Point&& pPoint);
+    void addWidget(Widget* pWidget, Position&& position);
 
     /**
      * Remove Widget From Layout Container.
      * @param pWidget widget
-     * @param pPoint position
      */
-    void removeWidget(Widget* pWidget, Point&& pPoint);
+    void removeWidget(Widget* pWidget);
 
     AbsolutelyLayout();
-    explicit AbsolutelyLayout(Page* page) : Layout(page){};
+    explicit AbsolutelyLayout(Page* page);
 
     ~AbsolutelyLayout() override = default;
     ;
--- main/src/layouts/absolutely_layout.cc
+++ main/src/layouts/absolutely_layout.cc
@@ -3,22 +3,90 @@
 //
 
 #include "absolutely_layout.h"
+#include "page.h"
 
 void oled::AbsolutelyLayout::addWidget(oled::Widget *pWidget,
-                                       oled::Point &&pPoint)
+                                       oled::Position &&point)
 {
-    Layout::addWidget(reinterpret_cast<oled::Widget *>(pWidget));
-    Layout::addPosition(std::move(pPoint));
+    if (point.getX() >= this->m_position.getX() &&    // X in rang
+        ((pWidget->getWidth() + point.getX()) <=      // left in rang
+         (this->m_position.getX() + this->m_size.getWidth())) &&
+        (point.getY() >= this->m_position.getY() &&    // Exec
+         ((pWidget->getHeight() + point.getY()) <=
+          (this->m_position.getY() + this->m_size.getHeight()))))
+    {
+        m_objects.emplace(std::make_pair(point, pWidget));
+    }
 }
 
-void oled::AbsolutelyLayout::removeWidget(oled::Widget *pWidget,
-                                          oled::Point &&pPoint)
+void oled::AbsolutelyLayout::removeWidget(oled::Widget *pWidget)
 {
-    Layout::removeWidget(pWidget);
-    Layout::removePosition(std::move(pPoint));
+    for (auto c = m_objects.begin(); c != m_objects.end(); c++)
+    {
+        if (c->second == pWidget)
+        {
+            m_objects.erase(c);
+            break;
+        }
+    }
 }
 
-oled::AbsolutelyLayout::AbsolutelyLayout()
+oled::AbsolutelyLayout::AbsolutelyLayout(oled::Page *page)
+    : Layout(page), m_objects()
+{
+}
+
+oled::AbsolutelyLayout::AbsolutelyLayout() : m_objects()
 {
     m_pPage = nullptr;
 }
+
+void oled::AbsolutelyLayout::setPage(oled::Page *pPage)
+{
+    Layout::setPage(pPage);
+
+    for (auto object : this->m_objects)
+    {
+        // ergodic widget and point to flash
+        object.second->setPage(pPage);
+    }
+}
+
+void oled::AbsolutelyLayout::flash()
+{
+    for (auto object : this->m_objects)
+    {
+        object.second->flash(this->m_pPage->dataMap(), object.first);
+    }
+}
+
+void oled::AbsolutelyLayout::flash(const oled::Widget *pWidget)
+{
+    for (auto object : this->m_objects)
+    {
+        if (pWidget == object.second)
+        {
+            object.second->flash(this->m_pPage->dataMap(), object.first);
+        }
+    }
+}
+
+// void oled::Layout::flash()
+//{
+//     for (auto compose : this->m_compose)
+//     {
+//         compose.first->flash(this->m_pPage->dataMap(), compose.second);
+//     }
+// }
+//
+// void oled::Layout::flash(const oled::Widget *pWidget)
+//{
+//     //    ESP_ERROR_CHECK(this->m_compose.size() !=
+//     this->m_positions.size()); for (auto compose : this->m_compose)
+//     {
+//         if (pWidget == compose.first)
+//         {
+//             compose.first->flash(this->m_pPage->dataMap(), compose.second);
+//         }
+//     }
+// }
