8258780594ca549193437a12e26f1db830722d86
monoliths <monoliths-uni@outlook.com>
2022-12-01 17:12:50 +0800

    resolved project error

--- inc/animation.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by monoliths on 11/27/22.
-//
-// Feature: use global player Queue, new A Task to complete
-
-#ifndef ESP32_C3_IIC_OLED_ANIMATION_H
-#define ESP32_C3_IIC_OLED_ANIMATION_H
-
-#endif    // ESP32_C3_IIC_OLED_ANIMATION_H
